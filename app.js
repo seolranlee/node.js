a82b732c436f2750e62d18d088d90ce02fc0dad8
seolranlee <leesugar90@gmail.com>
2018-10-23 16:23:41 +0900

    [node.js] - express를 이용해 간단한 웹 에플리케이션 만들기   + 엔트리 파일(app.js) 생성   + 정적파일을 서비스

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
+
+// get 을 '라우터'라고 부른다.
+// get 이 하는 일을 '라우팅'이라고 부른다.
+
+app.get('/', function(req, res){
+    res.send('Hello home page');
+});
+
+app.get('/route', function(req, res){
+    res.send('Hello Router, <img src="/route.png">');
+});
+
+app.get('/login', function(req, res){
+    res.send('<h1>Login please</h1>');
+});
 app.listen(3000, function(){
     console.log('Connected 3000 port!');
 });
