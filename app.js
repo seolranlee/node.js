0771578bdacf09b66c9df2599b7f06b299c965b5
seolranlee <leesugar90@gmail.com>
2018-10-23 17:50:24 +0900

    [node.js] - express를 이용해 간단한 웹 에플리케이션 만들기   + 엔트리 파일(app.js) 생성   + Express-정적파일을 서비스   + Express-웹페이지 표현법   + Express-템플릿 엔진 (Pug)

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+app.locals.pretty = true;
+// template 엔진을 명시한다.
+app.set('view engine', 'pug');
+// template가 있는 디렉토리를 알려준다.
+// 명시하지 않아도 jade는 views 디렉토리를 템플릿 디렉토리로 찾긴한다.
+app.set('views', './views');
 app.use(express.static('public'));
-
+app.get('/template', function(req, res){
+    // views안에 temp파일을 불러들인다.
+    // 두번째 인자에 템플릿 안에서 사용할 데이터를 '객체'로 전달할 수 있다.
+    res.render('temp', {time: Date(), _title: 'Jade'});
+});
 // get 을 '라우터'라고 부른다.
 // get 이 하는 일을 '라우팅'이라고 부른다.
 
