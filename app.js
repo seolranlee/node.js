160dd41149e2b792c3d4b999f4051d78a5d78327
seolranlee <leesugar90@gmail.com>
2018-10-23 16:56:49 +0900

    [node.js] - express를 이용해 간단한 웹 에플리케이션 만들기   + 엔트리 파일(app.js) 생성   + Express-정적파일을 서비스   + Express-웹페이지 표현법

--- app.js
+++ app.js
@@ -8,6 +8,33 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
     res.send('Hello home page');
 });
+app.get('/dynamic', function(req, res){
+    var lis = '';
+    for(var i=0; i<5; i++){
+        lis = lis + '<li>coding '+i+'</li>';
+    }
+    var time = Date();
+    var output = `
+    <!doctype html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    </head>
+    <body>
+    Hello, Dynamic
+    <ul>
+        ${lis}    
+    </ul>
+    ${time}
+    </body>
+    </html>
+    `;
+    res.send(output);
+});
 
 app.get('/route', function(req, res){
     res.send('Hello Router, <img src="/route.png">');
