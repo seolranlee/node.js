0e0577b37460305e54a4e25a181ae99a1a8afa5d
seolranlee <leesugar90@gmail.com>
2018-10-25 11:04:12 +0900

    [node.js] - express를 이용해 간단한 웹 에플리케이션 만들기   + 엔트리 파일(app.js) 생성   + Express-정적파일을 서비스   + Express-웹페이지 표현법   + Express-템플릿 엔진 (Pug)   + Express-POST 방식을 이용한 정보의 전달

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+// get방식의 데이터에는 express 자체적으로 접근할 수 있지만, post방식은 그렇게 하지 못해서 bodyParser라는 미들웨어가 필요하다.
+const bodyParser = require('body-parser');
 const app = express();
 app.locals.pretty = true;
 // template 엔진을 명시한다.
@@ -6,8 +8,29 @@ app.set('view engine', 'pug');
 // template가 있는 디렉토리를 알려준다.
 // 명시하지 않아도 jade는 views 디렉토리를 템플릿 디렉토리로 찾긴한다.
 app.set('views', './views');
-app.use(express.static('public'));
 
+app.use(express.static('public'));
+// use라는 메소드를 통해 bodyParser를 어플리케이션과 연결시킴.
+// 라우팅을 하기 전에 진행되는 일.
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/form', function(req, res){
+    res.render('form');
+});
+
+// form 태그는 생각해보면 url 을 생성해주는 작은 프로그램이라고 할 수 있다.
+app.get('/form_receiver', function(req, res){
+    var title = req.query.title;
+    var description = req.query.description;
+    res.send(title+','+description)
+});
+
+// bodyParser는 req객체에 body객체를 추가해줌.
+app.post('/form_receiver', function(req, res){
+    var title = req.body.title; // req객체의 body라는 객체의 title이라는 프로퍼티.
+    var description = req.body.description;
+    res.send(title+','+description);
+});
 // 시맨틱url 방식
 app.get('/topic/:id', function(req, res){
     var topics = [
@@ -16,14 +39,31 @@ app.get('/topic/:id', function(req, res){
         'Express is...'
     ];
     var output = `
-        <a href="/topic?id=0">JavaScript</a><br>
-        <a href="/topic?id=1">Nodejs</a><br>
-        <a href="/topic?id=2">Express</a><br><br>
+        <a href="/topic/0">JavaScript</a><br>
+        <a href="/topic/1">Nodejs</a><br>
+        <a href="/topic/2">Express</a><br><br>
         ${topics[req.params.id]}
     `
-    // query스트링
     res.send(output);
 });
+
+// query string 방식
+// app.get('/topic', function(req, res){
+//     var topics = [
+//         'Javascript is...',
+//         'Nodejs is...',
+//         'Express is...'
+//     ];
+//     var output = `
+//         <a href="/topic?id=0">JavaScript</a><br>
+//         <a href="/topic?id=1">Nodejs</a><br>
+//         <a href="/topic?id=2">Express</a><br><br>
+//         ${topics[req.query.id]}
+//     `
+//
+//     res.send(output);
+// });
+
 app.get('/topic/:id/:mode', function(req, res){
    res.send(req.params.id +','+ req.params.mode)
 });
