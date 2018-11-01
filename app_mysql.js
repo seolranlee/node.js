3dc46d760dcbed1a493949b82b20fcf135e885c6
seolranlee <leesugar90@gmail.com>
2018-11-01 16:25:47 +0900

    [node.js + MySQL] - MySQL로 웹에플리케이션 구현   + 글 목록 구현(SELECT)   + 글 상세보기 구현(SELECT)   + 글 추가기능 구현(INSERT)   + 글 편집기능 구현(UPDATE)   + 글 삭제기능 구현(DELETE)

--- app_mysql.js
+++ app_mysql.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 // 미들웨어
 var _storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -10,10 +10,9 @@ var _storage = multer.diskStorage({
         cb(null, file.originalname)
     }
 });
-const upload = multer({ storage: _storage });
-const fs = require('fs');
-const mysql      = require('mysql');
-const conn = mysql.createConnection({
+var upload = multer({ storage: _storage });
+var mysql      = require('mysql');
+var conn = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '1111',
@@ -21,18 +20,13 @@ const conn = mysql.createConnection({
 });
 
 conn.connect();
-const app = express();
+var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
 app.use('/user', express.static('uploads'));
 app.set('views', './views_mysql');
 app.set('view engine', 'pug');
-
-// 2. 라우팅
-app.get('/upload', (req, res)=>{
-   res.render('upload');
-});
 
 // 뒤에 콜백함수가 실행되기 전에 미들웨어(upload.single())가 실행된다.
 // 이 미들웨어는 req객체 안에 file이라는 객체를 붙여준다.
@@ -41,24 +35,114 @@ app.post('/upload', upload.single('userfile'), function (req, res) {
     res.send('Uploaded : '+req.file.filename);
 });
 
-app.get('/topic/new', function(req, res){
-    fs.readdir('data/', (err, files)=> {
-        if (err) {
+app.get('/topic/add', function(req, res){
+    var sql = 'SELECT id, title FROM topic';
+    conn.query(sql, function(err, topics, fields){
+      if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+      }
+        res.render('add', {topics: topics});
+    })
+});
+
+app.post('/topic/add', function(req, res){
+    var title = req.body.title;
+    var description = req.body.description;
+    var author = req.body.author;
+
+    var sql = 'INSERT INTO topic (title, description, author) VALUES (?, ?, ?)';
+    conn.query(sql, [title, description, author], function(err, result, fields){
+        if(err){
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
-        res.render('new', {topics: files});
-
-    });
+        res.redirect('/topic/'+result.insertId);
+    })
 });
-app.get(['/topic', '/topic/:id'], (req, res)=>{
 
+app.get(['/topic/:id/edit'], function(req, res){
     var sql = 'SELECT id, title FROM topic';
     conn.query(sql, function(err, topics, fields){
         var id = req.params.id;
         if(id){
             var sql = 'SELECT * FROM topic WHERE id=?';
-            conn.query(sql,id, function(err, topic, fields){
+            conn.query(sql, [id], function(err, topic, fields){
+                if(err){
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                }else{
+                    res.render('edit',  {topics: topics, topic: topic[0]});
+                }
+
+            })
+        }else{
+            console.log('There is no id');
+            res.status(500).send('Internal Server Error');
+        }
+
+    });
+});
+
+app.post(['/topic/:id/edit'], function(req, res){
+
+    var title = req.body.title;
+    var description = req.body.description;
+    var author = req.body.author;
+    var id = req.params.id;
+
+    var sql = 'UPDATE topic SET title=?, description=?, author=? WHERE id=?';
+    conn.query(sql, [title, description, author, id], function(err, result, fields){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.redirect('/topic/'+id);
+    });
+});
+
+app.get('/topic/:id/delete', function(req, res){
+    var sql = 'SELECT id,title FROM topic';
+    var id = req.params.id;
+    conn.query(sql, [id], function(err, topics, fields){
+        var sql = 'SELECT * FROM topic WHERE id=?';
+        conn.query(sql, [id], function(err, topic){
+            if(err){
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            } else{
+                if(topic.length === 0){
+                    console.log('There is no record.');
+                    res.status(500).send('Internal Server Error');
+                }else{
+                    res.render('delete', {topics: topics, topic: topic[0]});
+                }
+            }
+        });
+
+    })
+});
+
+app.post('/topic/:id/delete', function(req, res){
+    var id = req.params.id;
+    var sql = 'DELETE FROM topic WHERE id=?';
+    conn.query(sql, [id], function(err, result){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.redirect('/topic');
+    })
+});
+
+
+app.get(['/topic', '/topic/:id'], function(req, res){
+    var sql = 'SELECT id, title FROM topic';
+    conn.query(sql, function(err, topics, fields){
+        var id = req.params.id;
+        if(id){
+            var sql = 'SELECT * FROM topic WHERE id=?';
+            conn.query(sql, [id], function(err, topic, fields){
                 if(err){
                     console.log(err);
                     res.status(500).send('Internal Server Error');
@@ -69,23 +153,12 @@ app.get(['/topic', '/topic/:id'], (req, res)=>{
 
             })
         }else{
+            console.log('test');
             res.render('view', {topics: topics});
         }
-
     });
 });
-app.post('/topic', function(req, res){
-    var title = req.body.title;
-    var description = req.body.description;
-    fs.writeFile('data/'+title, description, (err) => {
-        if(err) {
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        }
-        res.redirect('/topic/'+title);
-    });
 
-});
 // 1. 포트와 커넥션
 app.listen(3000, function(){
     console.log('Connected 3000 port!');
